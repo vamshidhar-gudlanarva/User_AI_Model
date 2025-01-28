dcfcf5ba4cd861ad6aa39293045586eee42ece43
vamshidhar <vamshidhar1001@gmail.com>
2025-01-28 12:06:39 -0800

    error fixed

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -6,22 +6,25 @@ from django.contrib.auth import get_user_model
 from django.urls import reverse
 from django.test import Client
 
+
 class AdminSiteTests(TestCase):
     """Test for Django admin."""
 
     def setUp(self):
-       """create user and client."""
-       self.client = Client()
-       self.admin_user = get_user_model().objects.create_superuser(
-           email='admin@example.com',
-           password='testpass123',
-       )
-       self.client.force_login(self.admin_user)
-       self.user = get_user_model().objects.create_user(
-           email='user@example.com',
-           password='testpass123',
-           name='Test User'
-       )
+        """create user and client."""
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@example.com',
+            password='testpass123',
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='user@example.com',
+            password='testpass123',
+            name='Test User'
+        )
+
+
 
     def test_user_list(self):
         """Test that users are listed on page."""
